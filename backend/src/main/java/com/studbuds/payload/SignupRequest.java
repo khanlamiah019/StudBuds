40017090d5916f5eda3ce1225bf49f522dd7b9eb
khanlamiah019 <khanlamiah017@gmail.com>
2025-03-02 23:12:39 -0500

    Update SignupRequest.java

--- backend/src/main/java/com/studbuds/payload/SignupRequest.java
+++ backend/src/main/java/com/studbuds/payload/SignupRequest.java
@@ -1,12 +1,15 @@
 package com.studbuds.payload;
 
 public class SignupRequest {
+   private String name;
    private String email;
    private String password;
    private String major;
    private String year;
    
    // Getters and setters
+   public String getName() { return name; }
+   public void setName(String name) { this.name = name; }
    public String getEmail() { return email; }
    public void setEmail(String email) { this.email = email; }
    public String getPassword() { return password; }
