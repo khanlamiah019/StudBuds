d410c28ec9c9847147d5358c7da534d574cf11b5
Meghan Kret <68866536+MeghanRN@users.noreply.github.com>
2025-03-10 22:41:08 +0000

    added support for the account deletion auth

--- backend/src/main/java/com/studbuds/repository/MatchRepository.java
+++ backend/src/main/java/com/studbuds/repository/MatchRepository.java
@@ -2,6 +2,9 @@ package com.studbuds.repository;
 
 import com.studbuds.model.Match;
 import com.studbuds.model.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +16,8 @@ public interface MatchRepository extends JpaRepository<Match, Long> {
     
     @Query("SELECT m FROM Match m WHERE (m.user1 = :user1 AND m.user2 = :user2) OR (m.user1 = :user2 AND m.user2 = :user1)")
     Optional<Match> findExistingMatch(@Param("user1") User user1, @Param("user2") User user2);
+    
+    @Query("SELECT m FROM Match m WHERE m.user1 = :user OR m.user2 = :user")
+    List<Match> findAllByUser(@Param("user") User user);
+    
 }
