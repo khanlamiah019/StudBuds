e008829595914255d5fc56bb9021d7075d68a1de
megvo <112580071+megvo@users.noreply.github.com>
2025-04-30 16:18:27 -0400

    Update SecurityConfig.java

--- backend/src/main/java/com/studbuds/config/SecurityConfig.java
+++ backend/src/main/java/com/studbuds/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:3000", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:3000", "http://localhost:3000",  "https://thankful-desert-098fcfa0f.6.azurestaticapps.net"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
