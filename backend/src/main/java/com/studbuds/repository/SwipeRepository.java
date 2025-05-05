f8e08b119a6dc8d66e5bc530ba0d5508bf7a36a3
megvo <112580071+megvo@users.noreply.github.com>
2025-05-04 21:27:49 -0400

    Update SwipeRepository.java

--- backend/src/main/java/com/studbuds/repository/SwipeRepository.java
+++ backend/src/main/java/com/studbuds/repository/SwipeRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Repository
 public interface SwipeRepository extends JpaRepository<Swipe, Long> {
-    List<Swipe> findByFromUser(User fromUser);  
-    List<Swipe> findByToUser(User toUser);     
+    List<Swipe> findByFromUser(User fromUser);
+    List<Swipe> findByToUser(User toUser);
+    List<Swipe> findByFromUserOrderByCreatedAtAsc(User fromUser); // ✅ for chronological sorting
 }
